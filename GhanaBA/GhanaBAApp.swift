df0ae6289b9c7847dc1046309824914cdd8a8dd8
Young Prodigy <malicksamah3@gmail.com>
2024-01-09 08:54:11 -0500

    Initial Commit

---
+++ GhanaBA/GhanaBAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhanaBAApp.swift
+//  GhanaBA
+//
+//  Created by Malick Moro-Samah on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GhanaBAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
